ba84c3f44499120a2483ef1c4c5cb0b083578796
Guilherme Carvalho <55836497+nibiroo@users.noreply.github.com>
2023-03-03 01:09:31 -0300

    [dev_gui] - Praticando GIT

--- backend-service/src/app.service.ts
+++ backend-service/src/app.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+<<<<<<< HEAD
     return 'LOL';
+=======
+    return '<div style="color: green"> MONEY!!! </div>';
+  }
+  getGui(parametro:string): string {
+    var texto = parametro;
+    return '<div style="color: green">'+
+              '<input type="button" onClick="alertaGui()" value="click here"/>'+
+            '</div>'+
+            '<script>'+
+              'function alertaGui()'+
+              '{ alert("'+texto+'");}'+
+            '</script>';
+>>>>>>> db89e39 ([dev_gui] - Praticando GIT)
   }
 }
